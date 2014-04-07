17988f72b7bfde959d918169228e0515a50309ec
Fendi <endytc@gmail.com>
2014-04-08 03:44:38 +0700

    cms article

--- modules/pageadmin/controllers/CmsArtikelController.php
+++ modules/pageadmin/controllers/CmsArtikelController.php
@@ -1,6 +1,6 @@
 <?php
 
-class CmsArtikelController extends AdminBlogController
+class CmsArtikelController extends MyAdminController
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
